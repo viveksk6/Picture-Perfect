7895c64ee6a144ab48c97fa907b5d9547e306447
viveksk6 <36484434+viveksk6@users.noreply.github.com>
2019-11-21 19:35:35 +0530

    Update main.go

--- Backend/main.go
+++ Backend/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"encoding/json"
-	"github.com/gorilla/mux"
 	"strconv"
 
 	//"github.com/davecgh/go-spew/spew"
@@ -37,8 +37,36 @@ func queryDb()  {
 	// defer the close till after the main function has finished
 	// executing
 	defer db.Close()
+
 	q := "SELECT movieId, title, summary, genre, img, language, certificate from movies limit "
 	qstring := q + pageNo + "," + pageSize
+	//qstring2:= qstring + " where title like %a%"
+	//qstring2:= "SELECT movieId, title, summary, genre, img, language, certificate from movies where title like '%a%'"
+	results, err := db.Query(qstring)
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	for results.Next() {
+		var md MovieDetails
+
+		err = results.Scan(&md.MovieId, &md.Title,  &md.Summary, &md.Genre, &md.Img, &md.Language, &md.Certificate)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		movieDetails = append(movieDetails, md)
+	}
+}
+
+func queryDbMovie(movieId string)  {
+	db, err := sql.Open("mysql", "clumio:qwerty123@tcp(127.0.0.1:3306)/pictureperfect")
+	if err != nil {
+		panic(err.Error())
+	}
+	// defer the close till after the main function has finished
+	// executing
+	defer db.Close()
+	q := "SELECT movieId, title, summary, genre, img, language, certificate from movies where movieId = "
+	qstring := q + "'" + movieId + "'"
 	results, err := db.Query(qstring)
 	if err != nil {
 		panic(err.Error()) // proper error handling instead of panic in your app
@@ -62,29 +90,52 @@ func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Endpoint Hit: homePage")
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request){
+func returnAllMovies(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	movieDetails = nil
+	pageSize = ""
+	pageNo = ""
 	queryValues := r.URL.Query()
 	pageSize = queryValues.Get("pageSize")
-	pageNoInt, err:= strconv.Atoi(queryValues.Get("pageNo"))
-	if err != nil {
-		panic(err.Error())
+	pageNo = queryValues.Get("pageNo")
+
+
+
+	if pageNo!="" && pageSize!="" {
+		pageNoInt, err:= strconv.Atoi(pageNo)
+		if err != nil {
+			panic(err.Error())
+		}
+		pageSizeInt, err:= strconv.Atoi(pageSize)
+		if err != nil {
+			panic(err.Error())
+		}
+		pageNo = strconv.Itoa(pageSizeInt*(pageNoInt-1))
+		queryDb()
+		fmt.Println("Endpoint Hit: returnAllMovies")
+		json.NewEncoder(w).Encode(movieDetails)
+	} else {
+		fmt.Fprintf(w, "Invalid query parameters")
 	}
-	pageSizeInt, err:= strconv.Atoi(pageSize)
-	if err != nil {
-		panic(err.Error())
-	}
-	pageNo = strconv.Itoa(pageSizeInt*(pageNoInt-1))
-	queryDb()
-	fmt.Println("Endpoint Hit: returnAllArticles")
+
+
+}
+
+func returnSingleMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	vars := mux.Vars(r)
+	movieId:= vars["movieId"]
+	movieDetails = nil
+	queryDbMovie(movieId)
+	fmt.Println("Endpoint Hit: returnSingleMovie")
 	json.NewEncoder(w).Encode(movieDetails)
 }
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/all", returnAllArticles).Methods("GET", "OPTIONS")
+	myRouter.HandleFunc("/all", returnAllMovies).Methods("GET", "OPTIONS")
+	myRouter.HandleFunc("/all/{movieId}", returnSingleMovie).Methods("GET", "OPTIONS")
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
